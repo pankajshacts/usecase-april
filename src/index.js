2f2a99c9cf2f4d92e4d8bd0c9b7bb69c48518a7a
Pankaj Sha <Pankaj.Sha2@cognizant.com>
2024-04-09 11:24:02 +0530

    fix: add log message

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import express from "express";
 import StatusCode from "http-status-codes";
 import errorHandler from "./utils/errorHandler.js";
 import apiRouter from "./routes/index.js";
-import {serverConfig, dbConnect} from "./config/index.js"
+import {serverConfig, dbConnect, logger} from "./config/index.js"
 
 function createApp(){
+    
+    logger.info("Setup express application")
     const PORT = serverConfig.PORT;
     const app = express();
 
+    logger.info("Setup middlewares");
     app.use(express.json());
     app.use(express.text());
     app.use(express.urlencoded({extended:true}));
 
+    logger.info("Setup api routes");
     app.use("/api", apiRouter);
 
     app.get("/healthcheck", function healtcheck(_req, res){
@@ -20,12 +24,17 @@ function createApp(){
         })
     })
 
+    logger.info("Setup error handler")
     app.use(errorHandler);
 
-    app.listen(PORT, async function start(){
-        console.log(`Server is running on PORT=${PORT}`);
-        await dbConnect();
-        console.log(`Database connected`);
+    dbConnect().then(()=>{
+        logger.info("Database connected");
+        app.listen(PORT, function start(){
+            logger.info(`Server is running on ${PORT}`);
+        });
+    }).catch((error)=>{
+        logger.error("Database connection failed");
+        logger.error(error);
     })
 }
 
