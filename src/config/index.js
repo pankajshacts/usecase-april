f07aed190e6122a72f78ff6b9e1491e6be4fe010
Sha <2141712@cognizant.com>
2024-04-09 10:46:03 +0530

    fix: export logger

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,9 @@
 import dbConnect from "./db.config.js";
 import serverConfig from "./server.config.js";
+import logger from "./logger.config.js";
 
 export {
     dbConnect, 
     serverConfig,
+    logger
 }
